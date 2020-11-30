fcdd8cd2d07f3a9ad1f0a2b8fa95e8903c48e7a6
= <=>
2020-11-30 22:39:22 +0700

    day filr web

---
+++ bandienthoai/admin/modules/footer.php
@@ -0,0 +1,7 @@
+        <section>
+        <div class="footer" style="position: fixed; bottom: 0px; width: 100%">
+                <div class="wthree-copyright" >
+                    <p>&copy; 2020 Hệ thống bán lẻ laptop</p>
+                </div>
+            </div>
+        </section>
---
+++ bandienthoai/modules/partner.php
@@ -0,0 +1,17 @@
+<div class="container" style="padding:20px 10px;">
+	<div class="row">
+		<div class="col-md-2 clearfix">
+			<img style="width: 180px; height: 60px;" src="upload/imgwb/logo_apple.jpg" alt="">
+		</div>
+		<div class="col-md-2 clearfix">
+			<img style="width: 180px; height: 60px;" src="upload/imgwb/logo_oppo.jpg" alt="">
+		</div>
+		<div class="col-md-2 clearfix">
+			<img style="width: 180px; height: 60px;" src="upload/imgwb/logo_samsung.PNG" alt="">
+		</div>
+		<div class="col-md-2 clearfix">
+			<img style="width: 180px; height: 60px;" src="upload/imgwb/ms.PNG" alt="">
+		</div>
+		
+	</div>
+</div>
